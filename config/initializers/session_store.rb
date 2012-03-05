db5e9b88174e329062372dc71b6ff1a29641879a
Nando Vieira <fnando.vieira@gmail.com>
2012-03-05 00:00:33 -0300

    Securing cookies.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,6 @@
-# Be sure to restart your server when you modify this file.
-
-Skeleton::Application.config.session_store :cookie_store, key: "_skeleton_session"
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn"t be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Skeleton::Application.config.session_store :active_record_store
+Skeleton::Application.config.session_store :cookie_store, {
+  :key          => "_skeleton_session",
+  :httponly     => true,
+  # :secure       => Rails.env.production?,
+  :expire_after => 24.hours
+}
